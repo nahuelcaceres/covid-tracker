e7c10d667eaed6d89db1ec9bff4c5f16c317eea0
nahuelcaceres <nahuelcaceres@users.noreply.github.com>
2020-07-29 22:15:45 -0300

    Updates

---
+++ precache-manifest.0c7efa57de80878781f27746e3b343b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770824ee1e8282bbf73ecad78a24eef3",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f0e7105171ba59d7ca66",
+    "url": "/covid-tracker/static/css/main.a0256eb5.chunk.css"
+  },
+  {
+    "revision": "c397de16864266d95d71",
+    "url": "/covid-tracker/static/js/2.fcbc137a.chunk.js"
+  },
+  {
+    "revision": "bbe2dbbdaea8525fcc46934c5c58f863",
+    "url": "/covid-tracker/static/js/2.fcbc137a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e7105171ba59d7ca66",
+    "url": "/covid-tracker/static/js/main.0193a499.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
