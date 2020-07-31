4be22dd6f06c05ad53d92331b2620678f89e414d
nahuelcaceres <nahuelcaceres@users.noreply.github.com>
2020-07-30 21:19:04 -0300

    adding min/max-with value to countries

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react'
 import {fetchCountries} from '../../api';
-import {FormControl, TextField} from '@material-ui/core';
+import {TextField} from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import styles from './CountryPicker.module.css';
@@ -8,7 +8,6 @@ import styles from './CountryPicker.module.css';
 const CountryPicker = ({handleCountryChange}) => {
     const [fetchedCountries, setFetchedCountries] = useState([]);
     
-
     useEffect(() => {
         const fetchAPI = async () => {
             setFetchedCountries(await fetchCountries());
@@ -23,19 +22,14 @@ const CountryPicker = ({handleCountryChange}) => {
     }
 
     return(
-            <FormControl className={styles.formControl}>
-                {/* <NativeSelect defaultValue="" onChange={(e) => (handleCountryChange(e.target.value))}>
-                    <option value="">Global</option>
-                    {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
-                </NativeSelect> */}
-                <Autocomplete
-                    id="combo-countries"
-                    onChange={onTagsChange}
-                    options={fetchedCountries}
-                    getOptionLabel={(country) => country}
-                    renderInput={(params) => <TextField {...params} label="Global" variant="outlined" />}
-                />
-            </FormControl>
+            
+            <Autocomplete className={styles.countries}
+                id="combo-countries"
+                onChange={onTagsChange}
+                options={fetchedCountries}
+                getOptionLabel={(country) => country}
+                renderInput={(params) => <TextField {...params} label="Global" variant="outlined" />}
+            />
     )
 }
 
